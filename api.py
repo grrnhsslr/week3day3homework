63e240d76c76f02818eba45a1df212cb77836f4c
grrnhsslr <hasslergarren@gmail.com>
2024-03-06 18:08:31 -0600

    added second file

---
+++ api.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class PokemonStats:
+    def __init__(self, name, weight, height):
+        self.name = name
+        self.weight = weight
+        self.height = height
+
+
+class PokemonAPI:
+    base_url = "https://pokeapi.co/api/v2/pokemon/"
+
+    def __init__(self):
+        pass
+
+    def get_pokemon(self, name):
+        poke_stat = requests.get(self.base_url + name)
+        if poke_stat:
+            data = poke_stat.json()
+            name = data['name']
+            height = data['height']
+            weight = data['weight']
+            pokemon = PokemonStats(name, height, weight)
+            return pokemon
+        else:
+            print('Pokemon not found')
--- homework.py
+++ homework.py
@@ -1,32 +1,14 @@
 # Create a python wrapper for the Pokémon API. It should take in a Pokémon name and create a Pokémon object.
 # The Pokémon object should have, at a minimum, the Pokémon's name, height, and weight.
 
-import requests
+from api import PokemonAPI
 
-
-class Pokemon:
-    base_url = "https://pokeapi.co/api/v2/pokemon/"
-
-    def __init__(self):
-        self.weight = None
-        self.height = None
-        self.name = None
-
-    def get_pokemon(self, name):
-        poke_stat = requests.get(self.base_url + name)
-        if poke_stat:
-            data = poke_stat.json()
-            self.name = data['name']
-            self.height = data['height']
-            self.weight = data['weight']
-        else:
-            print('Pokemon not found')
-
-
-pokemon1 = Pokemon()
-pokemon1.get_pokemon(input('Enter pokemon name: '))
-print(f'{pokemon1.name} is your Pokemon, it weighs {pokemon1.weight}, and is {pokemon1.height} units tall')
-
-pokemon2 = Pokemon()
-pokemon2.get_pokemon('bulbasaur')
-print(f'{pokemon2.name} is your Pokemon, it weighs {pokemon2.weight}, and is {pokemon2.height} units tall')
+pokemon = PokemonAPI()
+charizard_stats = pokemon.get_pokemon('charizard')
+bulb_stats = pokemon.get_pokemon('bulbasaur')
+print(charizard_stats.name)
+print(charizard_stats.weight)
+print(charizard_stats.height)
+print(bulb_stats.name)
+print(bulb_stats.weight)
+print(bulb_stats.height)
